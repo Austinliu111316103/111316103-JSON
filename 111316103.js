b90b72f5b5a29635e7f45be6a9f08099464ec733
Austinliu111316103 <168444495+Austinliu111316103@users.noreply.github.com>
2024-05-28 15:47:17 +0800

    Create 111316103.js

---
+++ 111316103.js
@@ -0,0 +1,20 @@
+[
+  {
+    "LOINC": "55424-6",
+    "Display": "Calories burned",
+    "valueQuantityUnit": "kilocalories",
+    "valueQuantityCode": "kcal"
+  },
+  {
+    "LOINC": "55411-3",
+    "Display": "Exercise duration",
+    "valueQuantityUnit": "minutes",
+    "valueQuantityCode": "min"
+  },
+  {
+    "LOINC": "55425-3",
+    "Display": "Mean heart rate",
+    "valueQuantityUnit": "beats per minute",
+    "valueQuantityCode": "/min"
+  }
+]
